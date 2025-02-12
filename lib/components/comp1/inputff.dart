ee2bd20ad9f34a66d7e25096b79cdf3e829f2b13
Anshumaankhare2403 <anshumaan24khare1@gmail.com>
2025-02-12 13:06:49 +0530

    ALL

--- lib/components/comp1/inputff.dart
+++ lib/components/comp1/inputff.dart
@@ -6,19 +6,32 @@ class Inputff extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
-        height: 65,
-        decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 233, 233, 233),
-            borderRadius: BorderRadius.circular(20)),
-        child: TextField(
-          decoration: InputDecoration(
-            prefixIcon: Icon(FontAwesomeIcons.magnifyingGlass),
-            hintText:
-                "Enter what you're looking for", // This stays inline with input
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.symmetric(vertical: 20),
+      height: screenHeight * 0.08, // 8% of screen height
+      width: screenWidth * 0.9, // 90% of screen width
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 233, 233, 233),
+        borderRadius:
+            BorderRadius.circular(screenWidth * 0.05), // Dynamic border radius
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+            child: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size:
+                  screenWidth * 0.06, // Adjust icon size based on screen width
+            ),
           ),
-        ));
+          hintText: "Enter what you're looking for",
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(vertical: screenHeight * 0.025),
+        ),
+      ),
+    );
   }
 }
--- lib/page/page_1.dart
+++ lib/page/page_1.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../components/comp1/inputff.dart';
@@ -12,77 +10,152 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+  List<String> arr = [
+    "assets/image/page1/page1 (2).jpg",
+    "assets/image/page1/page1 (4).jpg",
+    "assets/image/page1/page1 (1).jpg",
+    "assets/image/page1/page1 (5).jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 204, 199, 199),
+      backgroundColor: const Color.fromARGB(255, 233, 233, 233),
       body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // container 1
-          Container(
-            height: 320,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(35))),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      FontAwesomeIcons.bars,
-                      size: 35,
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      "Find You",
-                      style: TextStyle(
-                        fontSize: 30,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Top Container
+              Container(
+                height: screenHeight * 0.35, // 35% of screen height
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(screenWidth * 0.08),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: screenWidth * 0.06,
+                      vertical: screenHeight * 0.03),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(FontAwesomeIcons.bars, size: screenWidth * 0.08),
+                      SizedBox(height: screenHeight * 0.03),
+                      Text(
+                        "Find You",
+                        style: TextStyle(fontSize: screenWidth * 0.07),
                       ),
-                      textAlign: TextAlign.start,
-                    ),
-                    Text(
-                      "Inspiration",
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.w500,
+                      Text(
+                        "Inspiration",
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.09,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                      textAlign: TextAlign.start,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Inputff()
-                  ],
+                      SizedBox(height: screenHeight * 0.02),
+                      Inputff(),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: screenHeight * 0.02),
+
+              // Promo Title
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+                child: Text(
+                  "Promo Today",
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.06,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.015),
+
+              // Promo Images (Horizontal List)
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: screenHeight * 0.25,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: arr.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: screenWidth * 0.43,
+                            margin: EdgeInsets.symmetric(
+                                horizontal: screenWidth * 0.029),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(arr[index]),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius:
+                                  BorderRadius.circular(screenWidth * 0.08),
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(screenWidth * 0.08),
+                                gradient: LinearGradient(
+                                  begin: Alignment.bottomRight,
+                                  colors: [
+                                    Colors.black.withOpacity(0.8),
+                                    Colors.black.withOpacity(.01),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: screenHeight * 0.02),
+
+              // Large Promo Image
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+                child: Container(
+                  height: screenHeight * 0.2,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/image/page1/page1 (3).jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(screenWidth * 0.08),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(screenWidth * 0.08),
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        colors: [
+                          Colors.black.withOpacity(.8),
+                          Colors.black.withOpacity(.01),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          // container
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              "Promo Today",
-              style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          Expanded(child: )
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
