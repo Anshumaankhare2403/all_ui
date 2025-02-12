20d85d0b00d6a251ccb47db1b5674f05716dfaf6
Anshumaankhare2403 <anshumaan24khare1@gmail.com>
2025-02-12 18:39:49 +0530

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import "./page/page_1.dart";
+// import "./page/page_1.dart";
+import "./page/page_2.dart";
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Page1(),
+      home: Page2(),
     );
   }
 }
